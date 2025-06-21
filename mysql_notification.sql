922c20857446a5c9dad4c6dd0442d070b0872f24
mhasan8801 <hasanmuhammad197@gmail.com>
2025-06-21 20:36:09 +0700

    table notification

--- mysql_notification.sql
+++ mysql_notification.sql
@@ -18,7 +18,33 @@ INSERT INTO user (id, name) VALUES ("musa","Muhammad Hasan");
 
 SELECT * FROM user;
 
+-- Notification
+CREATE TABLE notification(
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(100) NOT NULL,
+    detail TEXT NOT NULL,
+    create_at TIMESTAMP NOT NULL,
+    user_id VARCHAR(100),
+    PRIMARY KEY (id)
+);
 
+ALTER TABLE notification -- Relasi user ke notif -> one to many
+ADD CONSTRAINT fk_notification_user
+FOREIGN KEY (user_id) REFERENCES user (id);
+
+DESC notification;
+
+INSERT INTO notification (title, detail, create_at, user_id)
+VALUES ("Judul Info", "Detail Info", CURRENT_TIMESTAMP(), "hasan"),
+		("Judul Promo", "Detail Promo", CURRENT_TIMESTAMP(), null),
+		("Judul Info 2", "Detail Info 2", CURRENT_TIMESTAMP(), "musa");
+
+SELECT * FROM notification;
+
+-- query show notification
+SELECT * FROM notification WHERE user_id = "hasan";
+SELECT * FROM notification WHERE (user_id = "hasan" OR user_id IS NULL);
+SELECT * FROM notification WHERE (user_id = "hasan" OR user_id IS NULL) ORDER BY create_at DESC;
 
 
 
