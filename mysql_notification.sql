b6feb5abb1a0a67a01f53db76f186dd380c6b655
mhasan8801 <hasanmuhammad197@gmail.com>
2025-06-21 20:34:25 +0700

    table user

--- mysql_notification.sql
+++ mysql_notification.sql
@@ -6,6 +6,18 @@ USE mysql_notification;
 
 SHOW TABLES;
 
+-- USER
+CREATE TABLE user (
+	id VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO user (id, name) VALUES ("hasan","M. Hasan");
+INSERT INTO user (id, name) VALUES ("musa","Muhammad Hasan");
+
+SELECT * FROM user;
+
 
 
 
